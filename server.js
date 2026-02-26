7bb9ece4da022f5423bcedb3607d031c2972d69f
Vijeta <vijetabaliyan3691@gmail.com>
2026-02-26 22:41:44 +0530

    Initial commit - Concurrent Ticket Booking with Redis

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { createClient } = require('redis');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+app.use(express.json());
+
+const redisClient = createClient();
+
+redisClient.connect()
+    .then(() => console.log("Redis Connected"))
+    .catch(err => console.error("Redis Error", err));
+
+const PORT = 3000;
+
+const seats = ["A1", "A2", "A3", "A4", "A5"];
+
+app.post('/book', async (req, res) => {
+    const { seat } = req.body;
+
+    if (!seats.includes(seat)) {
+        return res.status(400).json({ message: "Invalid seat" });
+    }
+
+    const lockKey = `lock:${seat}`;
+    const bookingKey = `booking:${seat}`;
+
+    const lock = await redisClient.set(lockKey, "locked", {
+        NX: true,
+        EX: 10
+    });
+
+    if (!lock) {
+        return res.status(409).json({ message: "Seat is being booked or already booked" });
+    }
+
+    const alreadyBooked = await redisClient.get(bookingKey);
+
+    if (alreadyBooked) {
+        await redisClient.del(lockKey);
+        return res.status(409).json({ message: "Seat already booked" });
+    }
+
+    const bookingId = uuidv4();
+    await redisClient.set(bookingKey, bookingId);
+
+    await redisClient.del(lockKey);
+
+    res.json({
+        message: "Seat booked successfully",
+        seat,
+        bookingId
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
