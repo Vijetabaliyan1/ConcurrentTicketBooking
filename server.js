6c6997fad7565db60a1141b2ff4d18f7100967fc
Vijeta <vijetabaliyan3691@gmail.com>
2026-03-05 13:22:08 +0530

    Added home route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 const redisClient = createClient({
-    url: process.env.REDIS_URL   // 🔥 important for deployment
+    url: process.env.REDIS_URL   // for Redis Cloud connection
 });
 
 redisClient.connect()
@@ -16,11 +16,22 @@ redisClient.connect()
 
 const PORT = process.env.PORT || 3000;
 
+// Available seats
 const seats = ["A1", "A2", "A3", "A4", "A5"];
 
+
+// 🔹 Home route (for demo link)
+app.get('/', (req, res) => {
+    res.send("Concurrent Ticket Booking API is running 🚀");
+});
+
+
+// 🔹 Book seat API
 app.post('/book', async (req, res) => {
+
     const { seat } = req.body;
 
+    // check valid seat
     if (!seats.includes(seat)) {
         return res.status(400).json({ message: "Invalid seat" });
     }
@@ -28,34 +39,46 @@ app.post('/book', async (req, res) => {
     const lockKey = `lock:${seat}`;
     const bookingKey = `booking:${seat}`;
 
+    // create temporary lock
     const lock = await redisClient.set(lockKey, "locked", {
         NX: true,
         EX: 10
     });
 
     if (!lock) {
-        return res.status(409).json({ message: "Seat is being booked or already booked" });
+        return res.status(409).json({
+            message: "Seat is being booked or already booked"
+        });
     }
 
+    // check if already booked
     const alreadyBooked = await redisClient.get(bookingKey);
 
     if (alreadyBooked) {
         await redisClient.del(lockKey);
-        return res.status(409).json({ message: "Seat already booked" });
+
+        return res.status(409).json({
+            message: "Seat already booked"
+        });
     }
 
+    // create booking
     const bookingId = uuidv4();
+
     await redisClient.set(bookingKey, bookingId);
 
+    // remove lock
     await redisClient.del(lockKey);
 
     res.json({
         message: "Seat booked successfully",
-        seat,
-        bookingId
+        seat: seat,
+        bookingId: bookingId
     });
 });
 
+
+// start server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
